21c514667b96e3f47c9811bd9ddc94be0862dcaf
d-h-k <ultraraptor@naver.com>
2021-12-24 01:30:31 +0900

    feat : Dto에서 엔티티로 변환하는 메서드 작성

--- src/main/java/com/board/post/dto/PostDtoRequest.java
+++ src/main/java/com/board/post/dto/PostDtoRequest.java
@@ -1,17 +1,29 @@
 package com.board.post.dto;
 
+import com.board.post.entity.Post;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import java.time.LocalDateTime;
 
+@Builder
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
 @Setter
 public class PostDtoRequest {
+
+    @Autowired
+    public PostDtoRequest(ModelMapper mapper) {
+        this.mapper = mapper;
+    }
+
     String title;
 
     String contents;
@@ -20,7 +32,12 @@ public class PostDtoRequest {
 
     LocalDateTime validUntil;
 
-    //@todo : author는 account정보에서 자동으로 읽어오기, DTO에서 안받고
+    @JsonIgnore
+    private ModelMapper mapper;
+
+    public Post toPost() {
+        return mapper.map(this, Post.class);
+    }
 
     //@todo : 첨부파일 여러개 필요
 }
