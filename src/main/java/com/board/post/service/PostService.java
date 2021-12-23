9233c551de1d3dc98667b416c72f87576b359a37
d-h-k <ultraraptor@naver.com>
2021-12-24 02:34:41 +0900

    feat : 업데이트 서비스레이어 작성

--- src/main/java/com/board/post/service/PostService.java
+++ src/main/java/com/board/post/service/PostService.java
@@ -6,6 +6,9 @@ import com.board.post.repository.PostRepository;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityNotFoundException;
 
 @Service
 @RequiredArgsConstructor
@@ -20,5 +23,14 @@ public class PostService {
         //@todo : author는 추후 account정보에서 자동으로 읽어오기, DTO에서 안받고 임시로 상수값으로 넣어줌
         post.config("Anonymous");
         return postRepository.save(post).getId();
+    }
+
+    @Transactional
+    public Long update(Long postId, PostDtoRequest request) {
+        Post savedPost = postRepository
+                .findById(postId)
+                .orElseThrow(EntityNotFoundException::new);
+
+        return postRepository.save(savedPost.update(request.toPost())).getId();
     }
 }
