c0b545f0c01429d4443e6efb03d37e834666bc3d
d-h-k <ultraraptor@naver.com>
2021-12-24 02:33:25 +0900

    feat : 모델맵퍼가 Setter 없이도 값을 셋팅할수 있도록 옵션 수정

--- src/main/java/com/board/config/ModelMapperConfig.java
+++ src/main/java/com/board/config/ModelMapperConfig.java
@@ -11,8 +11,10 @@ public class ModelMapperConfig {
     @Bean
     public ModelMapper modelMapper() {
         ModelMapper modelMapper = new ModelMapper();
-        /*연결 전략 : 같은 타입의 필드명이 같은 경우만 동작*/
-        modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
+
+        modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT) // 연결 전략 : 같은 타입의 필드명이 같은 경우만 동작
+                .setFieldAccessLevel(org.modelmapper.config.Configuration.AccessLevel.PRIVATE) // 롬복 세터를 지정하지 않아도 동작하도록
+                .setFieldMatchingEnabled(true);
         return modelMapper;
     }
 }
