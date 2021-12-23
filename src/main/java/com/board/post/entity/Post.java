43bdada96e2fe193d65f40a78383db9921f952cd
d-h-k <ultraraptor@naver.com>
2021-12-23 16:26:11 +0900

    feat : 기본 동작 테스트 - 엔티티에 최소한의 동작 확인을 위한 생성자와 필드 추가

--- src/main/java/com/board/post/entity/Post.java
+++ src/main/java/com/board/post/entity/Post.java
@@ -1,14 +1,22 @@
 package com.board.post.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
 @Entity
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Post {
 
     @Id
     @GeneratedValue
     Long id;
 
+    String title;
 }
