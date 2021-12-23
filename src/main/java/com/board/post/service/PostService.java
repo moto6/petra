dd2475ae87a7da2ac17ccab9e913849613f80564
d-h-k <ultraraptor@naver.com>
2021-12-24 01:31:13 +0900

    feat : 엔티티 저장하는 서비스 레이어 코드 구현

--- src/main/java/com/board/post/service/PostService.java
+++ src/main/java/com/board/post/service/PostService.java
@@ -1,11 +1,24 @@
 package com.board.post.service;
 
+import com.board.post.dto.PostDtoRequest;
+import com.board.post.entity.Post;
 import com.board.post.repository.PostRepository;
 import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class PostService {
+
     private final PostRepository postRepository;
+
+    private final ModelMapper mapper;
+
+    public Long save(PostDtoRequest request) {
+        Post post = request.toPost();
+        //@todo : author는 추후 account정보에서 자동으로 읽어오기, DTO에서 안받고 임시로 상수값으로 넣어줌
+        post.config("Anonymous");
+        return postRepository.save(post).getId();
+    }
 }
