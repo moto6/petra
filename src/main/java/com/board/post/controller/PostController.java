89dff34837e4ed59ca9781ca3763c6a4c3f7a865
d-h-k <ultraraptor@naver.com>
2021-12-23 16:26:27 +0900

    feat : 기본 동작 테스트 - 컨트롤러에 핸들러 메서드 추가

--- src/main/java/com/board/post/controller/PostController.java
+++ src/main/java/com/board/post/controller/PostController.java
@@ -1,7 +1,9 @@
 package com.board.post.controller;
 
+import com.board.post.entity.Post;
 import com.board.post.service.PostService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,5 +11,10 @@ import org.springframework.web.bind.annotation.RestController;
 public class PostController {
 
     private final PostService postService;
+
+    @GetMapping()
+    public Post hi () {
+        return new Post(11L,"hello world");
+    }
 
 }
