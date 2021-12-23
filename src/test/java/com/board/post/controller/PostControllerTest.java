12fc72c310ff7057158315a77a8a2f6352df9434
d-h-k <ultraraptor@naver.com>
2021-12-24 02:35:57 +0900

    feat : 업데이트 기능을 테스트하는 코드 작성

--- src/test/java/com/board/post/controller/PostControllerTest.java
+++ src/test/java/com/board/post/controller/PostControllerTest.java
@@ -1,21 +1,29 @@
 package com.board.post.controller;
 
-import com.board.post.dto.PostDtoRequest;
+import com.board.post.entity.Post;
+import com.board.post.repository.PostRepository;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.junit.jupiter.api.AfterEach;
+import org.apache.catalina.session.PersistentManager;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityNotFoundException;
+import javax.persistence.PersistenceContext;
 
 import static com.board.post.dto.PostDtoRequestTest.request1;
-import static org.junit.jupiter.api.Assertions.*;
+import static com.board.post.dto.PostDtoRequestTest.request2;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
@@ -28,11 +36,20 @@ class PostControllerTest {
     @Autowired
     private ObjectMapper objectMapper;
 
+    @Autowired
+    private ModelMapper modelMapper;
+
+    @Autowired
+    private PostRepository postRepository;
+
+    @PersistenceContext
+    EntityManager em;
+
     private static final String PREFIX = "/api/v1/post";
 
     @Test
     @DisplayName("글이 작성된다")
-    public void posting() throws Exception{
+    public void posting() throws Exception {
         //given
         String requestBody = objectMapper.writeValueAsString(request1);
 
@@ -45,6 +62,34 @@ class PostControllerTest {
                 )
                 .andDo(print())
                 .andExpect(status().isOk());     // 4
-                //.andExpect(content().string( "칩의 블로그입니다. chip"));
+        //.andExpect(content().string( "칩의 블로그입니다. chip"));
+    }
+
+    @Test
+    @DisplayName("글이 수정된다")
+    public void update() throws Exception {
+        //given
+        String requestBody = objectMapper.writeValueAsString(request2);
+
+        //when
+        Post post = modelMapper.map(request1, Post.class);
+        Post savedPost = postRepository.save(modelMapper.map(request1, Post.class));
+
+
+        //then
+        mockMvc.perform(put(PREFIX + "/" + savedPost.getId())
+                                .content(requestBody)
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .accept(MediaType.APPLICATION_JSON)
+                )
+                .andDo(print())
+                .andExpect(status().isOk());
+
+        //em.flush();
+        //em.clear();
+        Post afterUpdate = postRepository.findById(savedPost.getId()).orElseThrow(EntityNotFoundException::new);
+        assertThat(afterUpdate.getContents()).isEqualTo(request2.getContents());
+        assertThat(afterUpdate.getValidUntil()).isEqualTo(request2.getValidUntil());
+
     }
 }
