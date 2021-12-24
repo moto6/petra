666e4c984c8640c30f03abfb82af49e89afab056
d-h-k <ultraraptor@naver.com>
2021-12-24 15:43:32 +0900

    refactor : 코드 포매팅 정리

--- src/main/java/com/board/exception/ExceptionController.java
+++ src/main/java/com/board/exception/ExceptionController.java
@@ -7,14 +7,12 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
-import java.nio.file.AccessDeniedException;
-
 @ControllerAdvice
 public class ExceptionController {
     Logger log = LoggerFactory.getLogger(getClass());
 
     // OutOfDateException
-    @ExceptionHandler({ OutOfDateException.class })
+    @ExceptionHandler({OutOfDateException.class})
     public ResponseEntity<?> handleAccessDeniedException(final OutOfDateException e) {
         log.warn("OutOfDateException {}", e.getMessage());
         return ResponseEntity.status(HttpStatus.METHOD_NOT_ALLOWED).body(e.getMessage());
--- src/main/java/com/board/post/dto/PostListDtoResponse.java
+++ src/main/java/com/board/post/dto/PostListDtoResponse.java
@@ -1,6 +1,5 @@
 package com.board.post.dto;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
