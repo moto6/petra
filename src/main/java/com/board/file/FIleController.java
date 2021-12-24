dad60438ffe107f2795309cce772e813c272aa9b
d-h-k <ultraraptor@naver.com>
2021-12-24 23:19:45 +0900

    feat : 파일 업로드 서비스 구현을 위한 컨트롤러와 페이지 작성

--- src/main/java/com/board/file/FIleController.java
+++ src/main/java/com/board/file/FIleController.java
@@ -3,15 +3,30 @@ package com.board.file;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequiredArgsConstructor
 public class FIleController {
 
-    private FileService fileService;
+    private final FileService fileService;
 
-    @GetMapping
-    public String upload(){
+    @GetMapping("/upload")
+    public String upload() {
         return "upload";
+    }
+
+    @GetMapping("/success")
+    public String success() {
+        return "success";
+    }
+
+    @PostMapping("/api/v1/file")
+    public String uploadFile(@RequestParam("file") MultipartFile file) {
+        fileService.fileUpload(file);
+        return "redirect:/success";
     }
 }
