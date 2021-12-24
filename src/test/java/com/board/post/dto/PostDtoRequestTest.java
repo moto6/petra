32cbf65154674b1881eec01c2534647f1156ad08
d-h-k <ultraraptor@naver.com>
2021-12-24 15:13:19 +0900

    refactor : 데이터셋을 로그로 구별하기 더 쉽도록 네이밍 패턴으로 숫자를 앞에 붙임

--- src/test/java/com/board/post/dto/PostDtoRequestTest.java
+++ src/test/java/com/board/post/dto/PostDtoRequestTest.java
@@ -6,32 +6,32 @@ public class PostDtoRequestTest {
 
     public static final PostDtoRequest request1 = PostDtoRequest
             .builder()
-            .title("어린왕자")
-            .contents("작은별에 착륙하다")
+            .title("1_어린왕자")
+            .contents("1_작은별에 착륙하다")
             .validFrom(LocalDateTime.now())
-            .validUntil(LocalDateTime.MAX)
+            .validUntil(LocalDateTime.now().plusYears(100))
             .build();
 
     public static final PostDtoRequest request2 = PostDtoRequest
             .builder()
-            .title("해리포터 혼혈왕자")
-            .contents("스네이프교수님")
-            .validFrom(LocalDateTime.MIN)
-            .validUntil(LocalDateTime.now())
+            .title("2_해리포터 혼혈왕자")
+            .contents("2_스네이프교수님")
+            .validFrom(LocalDateTime.now().minusYears(100))
+            .validUntil(LocalDateTime.now().minusYears(1))
             .build();
 
     public static final PostDtoRequest request3 = PostDtoRequest
             .builder()
-            .title("스프링부트")
-            .contents("스프링은 IOC 컨테이너가 Bean 을 DI 해주는 OOP 프레임워크 입니다")
+            .title("3_스프링부트")
+            .contents("3_스프링은 IOC 컨테이너가 Bean 을 DI 해주는 OOP 프레임워크 입니다")
             .validFrom(null)
             .validUntil(null)
             .build();
 
     public static final PostDtoRequest request4 = PostDtoRequest
             .builder()
-            .title("JPA DDD AOC")
-            .contents("JPA-JPA-JPA-JPA-JPA-JPA-JPA-JPA-")
+            .title("4_JPA DDD AOC")
+            .contents("4_JPA-JPA-JPA-JPA-JPA-JPA-JPA-JPA-")
             .validFrom(null)
             .validUntil(LocalDateTime.now())
             .build();
