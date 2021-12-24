852f5672ca0382b8e61a467988b778a25f887a9f
d-h-k <ultraraptor@naver.com>
2021-12-24 23:20:30 +0900

    feat : 오타 수정

--- src/main/java/com/board/file/FileController.java
+++ src/main/java/com/board/file/FileController.java
@@ -10,7 +10,7 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequiredArgsConstructor
-public class FIleController {
+public class FileController {
 
     private final FileService fileService;
 
