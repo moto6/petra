fedd51fa420d724edeb03d4f392fbc4ec4da06ef
d-h-k <ultraraptor@naver.com>
2021-12-24 23:07:12 +0900

    feat : 파일 업로드 서비스 구현

--- src/main/java/com/board/file/FileService.java
+++ src/main/java/com/board/file/FileService.java
@@ -1,14 +1,42 @@
 package com.board.file;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 
 @Service
 public class FileService {
 
-    @Value("${user.home}}")
+    Logger log = LoggerFactory.getLogger(getClass());
+
+    @Value("${user.home}")
     private String uploadDir;
 
 
+    public void fileUpload(MultipartFile multipartFile) {
+        Path serverPath = Paths.get(
+                uploadDir +
+                        File.separator +
+                        StringUtils.cleanPath(multipartFile.getOriginalFilename()));
+
+        try {
+            Files.copy(multipartFile.getInputStream(), serverPath, StandardCopyOption.REPLACE_EXISTING);
+        } catch (IOException e) {
+            log.error("fail to store file : name={}, exception={}",
+                      multipartFile.getOriginalFilename(),
+                      e.getMessage());
+            throw new FileStorageException("fail to store file");
+        }
+    }
 
 }
