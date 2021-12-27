a3b4218dc0ceaf9e9dc0e77bd0c1278c109d6c11
d-h-k <ultraraptor@naver.com>
2021-12-28 08:26:14 +0900

    feat : 첨부파일 기능을 위한 엔티티 구현

--- src/main/java/com/board/attachfile/attachFile.java
+++ src/main/java/com/board/attachfile/attachFile.java
@@ -1,12 +1,16 @@
 package com.board.attachfile;
 
+import com.board.post.entity.Post;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 @Table(name = "attach_file")
@@ -20,8 +24,19 @@ public class attachFile {
     @Column(name = "attach_file_id")
     private Long id;
 
+    @Column(name = "original_file_name")
     private String originalFileName;
 
-    private String
+    @Column(name = "verified_file_name")
+    private String VerifiedFileName;
+
+    @ManyToOne(fetch = FetchType.LAZY) //N-1 단방향 관계로 맵핑
+    @JoinColumn(name = "post_id")
+    private Post post;
+
+    public String getImgUrl() {
+        return "not yet";
+        //스프링설정에서 읽어온 url에서 스테틱 제공
+    }
 
 }
