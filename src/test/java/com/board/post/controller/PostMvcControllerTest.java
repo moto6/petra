c69f8f6c12fec90cc623eb2318be1387fa5ceefc
d-h-k <ultraraptor@naver.com>
2021-12-30 02:08:11 +0900

    feat : 첨부파일과 요청을 동시에 발송하는 테스트코드 작성(실패하는 테스트)

--- src/test/java/com/board/post/controller/PostMvcControllerTest.java
+++ src/test/java/com/board/post/controller/PostMvcControllerTest.java
@@ -1,16 +1,25 @@
 package com.board.post.controller;
 
+import com.board.post.dto.PostDtoRequest;
 import com.board.post.entity.Post;
 import com.board.post.repository.PostRepository;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static com.board.post.dto.PostDtoRequestTest.request1;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
@@ -47,6 +56,40 @@ class PostMvcControllerTest {
         //when
         //then
         mockMvc.perform(get(URI)).andExpect(status().isOk());
+    }
+
+    @Disabled
+    @Test
+    @DisplayName("uploadFile 검증")
+    public void uploadFile() throws Exception{
+
+        //given
+        MockMultipartFile jsonFile = new MockMultipartFile("test.json", "", "application/json", "{\"key1\": \"value1\"}".getBytes());
+        PostDtoRequest request = request1;
+        List<MultipartFile> files = createMultipartFiles();
+
+        //when
+        //then
+        mockMvc.perform(MockMvcRequestBuilders.multipart("/fileUpload")
+                                .file("file", jsonFile.getBytes())
+                                .content("need content")
+                                .characterEncoding("UTF-8"))
+                .andExpect(status().isOk());
+    }
+
+    private List<MultipartFile> createMultipartFiles() throws Exception {
+
+        List<MultipartFile> multipartFileList = new ArrayList<>();
+
+        for (int i = 0; i < 2; i++) {
+            String path = "/";
+            String imageName = "image" + i + ".jpg";
+            MockMultipartFile multipartFile =
+                    new MockMultipartFile(path, imageName, "file", new byte[]{1, 2, 3, 4});
+            multipartFileList.add(multipartFile);
+        }
+
+        return multipartFileList;
     }
 
 }
