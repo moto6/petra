18f86f690a487eb95c0f989a73ccbbb7ace04db8
d-h-k <ultraraptor@naver.com>
2021-12-29 18:21:09 +0900

    feat : 레디스 서비스 테스트코드 뼈대 추가

---
+++ src/test/java/com/board/post/service/PostViewCountServiceTest.java
@@ -0,0 +1,19 @@
+package com.board.post.service;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class PostViewCountServiceTest {
+
+    @Test
+    @DisplayName(" ")
+    public void test() {
+        //given
+
+        //when
+
+        //then
+        throw new AssertionError();
+
+    }
+}
