e3c72ad5e8491c3a905eaaf67b7a1ca79c556d88
d-h-k <ultraraptor@naver.com>
2021-12-29 18:03:08 +0900

    feat : 불필요한 공백제거

--- src/main/java/com/board/BoardApplication.java
+++ src/main/java/com/board/BoardApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BoardApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(BoardApplication.class, args);
     }
-
 }
