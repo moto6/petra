1202a3142c9347cdcae2df7add983f0d20a693d7
d-h-k <ultraraptor@naver.com>
2022-01-16 19:22:06 +0900

    feat : 레디스로 Insert querY 문 출력 횟수를 줄이는 기능 구현 중..

--- src/main/java/com/board/post/service/PostViewCountService.java
+++ src/main/java/com/board/post/service/PostViewCountService.java
@@ -1,49 +1,75 @@
 package com.board.post.service;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
+import com.board.config.ViewCountRedisHandler;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import java.time.ZonedDateTime;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.concurrent.TimeUnit;
+import java.time.LocalDateTime;
 
 @Slf4j
 @Service
 @RequiredArgsConstructor
 public class PostViewCountService {
 
-    private static final Map<Long, ViewCount> redisSimulation = new HashMap<>();
+    @Value("${viewCountLazyUpdatePeriodSecond}")
+    Integer INTERVAL;
 
     //<Key:postId / Value:Time>
-    private final RedisTemplate<Long, ZonedDateTime> timer;
+    private final RedisTemplate<Long, ViewCountRedisHandler> timer;
 
-    //<Key:postId / Value:Count>
-    private final RedisTemplate<Long, Integer> counter;
 
-    private final PostService postService;
+    private final PostServiceImpl postServiceImpl;
 
     public void intervalCount(Long postId) {
-        //byRedis(postId); //레디스 Key expired 기능으로 구현하려고 했으나 실패
-        byMillis(postId);
+
+        if (isElapsed(postId, INTERVAL)) {
+            applyCount(postId);
+            return;
+        }
+        caching(postId);
+
+
     }
 
-
-    public void byRedis(Long postId) {
-        if (!isContained(postId)) {
-            postService.addViews(postId, getCount(postId));
-            identifier.opsForValue().set(postId, postId, 1, TimeUnit.SECONDS);
-            identifier.expire(postId, 1, TimeUnit.SECONDS);
-            counter.opsForValue().set(postId, 1L);
-            log.info("업데이트");
-        } else {
-            log.info("캐싱");
-            counter.opsForValue().set(postId, getCount(postId) + 1L);
+    private void caching(Long postId) {
+        if(containsKey(postId)) {
+            //값만 더하기
+            timer.opsForValue().get(postId).increment();
+            return;
         }
+
+    }
+
+    private boolean containsKey(Long postId) {
+        ViewCountRedisHandler handler = timer.opsForValue().get(postId);
+        if(handler == null) {
+            return false;
+        }
+        return true;
+    }
+
+    @Transactional
+    void applyCount(Long postId) {
+        postServiceImpl.incrementViewCount(postId, getCount(postId));
+        timer.opsForValue().decrement(postId);
+
+    }
+
+    public boolean isElapsed(Long postId, Integer interval) {
+        LocalDateTime standardTime = timer
+                .opsForValue()
+                .get(postId)
+                .getTime();
+        LocalDateTime currentTime = LocalDateTime.now();
+        return standardTime.plusSeconds(interval).isAfter(currentTime);
+
+        //
+        //timer.opsForValue().set(postId, LocalDateTime.now());
+        //.//return false;
     }
 
 
@@ -55,17 +81,5 @@ public class PostViewCountService {
         return 1;
     }
 
-    private boolean isContained(Long postId) {
-        Long sample = identifier.opsForValue().get(postId);
-        return identifier.opsForValue().get(postId) != null;
-
-    }
-
-    @AllArgsConstructor
-    @Getter
-    public class ViewCount {
-        private long millis;
-        private long upCount;
-    }
 
 }
