058a675ed5988de921a242ec760f42bf481ae287
d-h-k <ultraraptor@naver.com>
2022-01-17 12:20:52 +0900

    feat : 잘못된 검색조건 입력시 발생하는 예외를 처리하는 ExceptionHandler 추가

--- src/main/java/com/board/exception/ExceptionControllerAdvice.java
+++ src/main/java/com/board/exception/ExceptionControllerAdvice.java
@@ -1,5 +1,6 @@
 package com.board.exception;
 
+import com.board.common.ApiResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
