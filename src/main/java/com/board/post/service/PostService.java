1184d8aa4eb37624b7740c0cb62d5e26ff4ad994
d-h-k <ultraraptor@naver.com>
2022-01-17 09:36:24 +0900

    feat : 코드 포멧팅에 맞게 수정

--- src/main/java/com/board/post/service/PostService.java
+++ src/main/java/com/board/post/service/PostService.java
@@ -3,15 +3,26 @@ package com.board.post.service;
 import com.board.post.dto.PostDtoRequest;
 import com.board.post.entity.Post;
 import org.springframework.data.domain.Pageable;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 
 public interface PostService {
     Long save(PostDtoRequest request);
+
     Long update(Long postId, PostDtoRequest request);
+
     void delete(Long postId);
+
     Post read(Long postId);
+
     Post readAny(Long postId);
+
     List<Post> readAll(Pageable pageable);
+
     List<Post> readAnyAll(Pageable pageable);
+
+    @Deprecated
+    void saveWithAttach(PostDtoRequest postDtoRequest, List<MultipartFile> attachFiles);
+
 }
