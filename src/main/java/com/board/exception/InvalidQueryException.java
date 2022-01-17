8dfc5a3caca2e9477efc2689efcdcd4467b8ead1
d-h-k <ultraraptor@naver.com>
2022-01-17 14:03:29 +0900

    refactor : 응답문자열을 다른 부분과 일관성있게 수정

--- src/main/java/com/board/exception/InvalidQueryException.java
+++ src/main/java/com/board/exception/InvalidQueryException.java
@@ -2,6 +2,6 @@ package com.board.exception;
 
 public class InvalidQueryException extends RuntimeException {
     public InvalidQueryException(String string) {
-        super("Invalid Query String : [" + string + "] , please check API guide or Contact us");
+        super("Invalid Query String [" + string + "] is unknown, please check API guide or Contact us");
     }
 }
