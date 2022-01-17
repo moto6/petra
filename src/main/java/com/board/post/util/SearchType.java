f79df1a8f55d1454d62fc9c9f1c197240e73f8fb
d-h-k <ultraraptor@naver.com>
2022-01-17 12:18:55 +0900

    feat : String > Enum 으로 형변환기능 추가
    - 타입안전 컨테이너를 적용해서 형변환

--- src/main/java/com/board/post/util/SearchType.java
+++ src/main/java/com/board/post/util/SearchType.java
@@ -1,13 +1,36 @@
 package com.board.post.util;
 
+import com.board.exception.InvalidQueryException;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public enum SearchType {
 
-    EVERY("every contents response option : ignoring any condition"),
-    SALE("only ON-SALE contents response option : filtered goods only");
+    EVERY("[EVERY] : every contents will be returned - ignore any condition"),
+    SALE("[SALE] : only ON-SALE contents response option - filtered goods only");
 
     final String description;
 
+    public static Map<String, SearchType> typeMapping = new HashMap<>();
+
+    static {
+        typeMapping.put(EVERY.name(), EVERY);
+        typeMapping.put(SALE.name(), SALE);
+    }
+
+
     SearchType(String description) {
         this.description = description;
+    }
+
+    public static SearchType SearchTypeAdaptor(String query) {
+        String filteredString = query.toUpperCase();
+
+        if(typeMapping.get(filteredString) == null) {
+            throw new InvalidQueryException(filteredString);
+        }
+
+        return typeMapping.get(filteredString);
     }
 }
