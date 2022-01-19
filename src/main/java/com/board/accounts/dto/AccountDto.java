b98f36b3756170951a00013ca2ab9cbe7ba7e343
d-h-k <ultraraptor@naver.com>
2022-01-19 11:32:18 +0900

    feat : dto 클래스 작성

---
+++ src/main/java/com/board/accounts/dto/AccountDto.java
@@ -0,0 +1,18 @@
+package com.board.accounts.dto;
+
+import com.board.accounts.entity.AccountRole;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class AccountDto {
+    private Long id;
+    private String email;
+    private Set<AccountRole> roles = new HashSet<>();
+}
