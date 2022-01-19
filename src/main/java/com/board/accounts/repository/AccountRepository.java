5c66fd50e9f4f26f089491fd8a7385dac79f2a3f
d-h-k <ultraraptor@naver.com>
2022-01-19 11:31:27 +0900

    feat : 계정 DB 접근 레이어 생성

---
+++ src/main/java/com/board/accounts/repository/AccountRepository.java
@@ -0,0 +1,12 @@
+package com.board.accounts.repository;
+
+import com.board.accounts.entity.Account;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface AccountRepository extends JpaRepository<Account, Long> {
+    Optional<Account> findByEmail(String email);
+}
