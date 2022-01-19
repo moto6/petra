64dbbe405a6a144e50b852181a36f0997200f475
d-h-k <ultraraptor@naver.com>
2022-01-19 17:56:20 +0900

    feat : 테스트시 권한 부여

--- src/test/java/com/board/attachfile/controller/AttachFileControllerTest.java
+++ src/test/java/com/board/attachfile/controller/AttachFileControllerTest.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
@@ -39,6 +40,7 @@ class AttachFileControllerTest {
 
 
     @Test
+    @WithMockUser(roles = "USER")
     @DisplayName("uploadFileHandler 메서드 검증")
     public void uploadFileHandler() throws Exception {
 
