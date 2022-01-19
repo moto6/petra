5824843621066bfa30336684dbf0a6f9740e6ffa
d-h-k <ultraraptor@naver.com>
2022-01-19 14:29:10 +0900

    feat : 시큐리티 설정

---
+++ src/main/java/com/board/config/SecurityConfig.java
@@ -0,0 +1,43 @@
+package com.board.config;
+
+import com.board.accounts.auth.CustomOAuth2UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@RequiredArgsConstructor
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final CustomOAuth2UserService customOAuth2UserService;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                //H2 콘솔 접근을 위해서
+                .csrf()
+                .disable()
+                .headers()
+                .frameOptions()
+                .disable()
+                //
+                .and()
+                .authorizeRequests()
+                //permit
+                .antMatchers()
+                .permitAll()
+                //auth need
+                .antMatchers()
+                .authenticated()
+                //로그아웃시 리디렉션 위치
+                .and()
+                .logout()
+                .logoutSuccessUrl("/")
+                //로그인시 설정 진입점
+                .and()
+                .oauth2Login()
+                .userInfoEndpoint()
+                .userService(customOAuth2UserService);//UserService 인터페이스 구현체
+    }
+}
