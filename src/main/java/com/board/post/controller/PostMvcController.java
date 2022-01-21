26f1426de11b1e2a1917a1a64d877ff4de78f597
d-h-k <ultraraptor@naver.com>
2022-01-21 10:57:01 +0900

    feat : 첨부파일 테스트코드 제거

--- src/main/java/com/board/post/controller/PostMvcController.java
+++ src/main/java/com/board/post/controller/PostMvcController.java
@@ -10,13 +10,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.multipart.MultipartFile;
-
-import javax.validation.Valid;
-import java.util.List;
 
 import static com.board.post.util.SearchType.SALE;
 
