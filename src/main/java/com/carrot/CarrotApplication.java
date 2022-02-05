664ab01bbebfcc9616531f96f817ff8dd576970a
d-h-k <ultraraptor@naver.com>
2022-02-05 17:15:59 +0900

    feat : 프로젝트 명 변경

--- src/main/java/com/carrot/CarrotApplication.java
+++ src/main/java/com/carrot/CarrotApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BoardApplication {
+public class CarrotApplication {
     public static void main(String[] args) {
-        SpringApplication.run(BoardApplication.class, args);
+        SpringApplication.run(CarrotApplication.class, args);
     }
 }
