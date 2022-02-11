6fb375a91c14d5c1c61c46dda8aa7b466e0c81d0
d-h-k <ultraraptor@naver.com>
2022-02-11 12:04:09 +0900

    feat : 페이지 호출 로그 출력

--- src/main/java/com/carrot/web/controller/WebController.java
+++ src/main/java/com/carrot/web/controller/WebController.java
@@ -1,9 +1,11 @@
 package com.carrot.web.controller;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@Slf4j
 @Controller
 @RequiredArgsConstructor
 public class WebController {
@@ -11,12 +13,14 @@ public class WebController {
     @GetMapping()
     public String main() {
 
+        log.info("메인페이지");
         return "web/main";
     }
 
     @GetMapping("/myLogin")
     public String login() {
 
+        log.info("로그인페이지");
         return "web/login";
     }
 
