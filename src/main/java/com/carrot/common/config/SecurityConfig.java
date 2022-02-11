5e3ad0cbec071dc38cadb6e7b7104698372ed091
d-h-k <ultraraptor@naver.com>
2022-02-11 12:03:01 +0900

    feat : 시큐리티 설정이 적용되지 않는 문제 해결

--- src/main/java/com/carrot/common/config/SecurityConfig.java
+++ src/main/java/com/carrot/common/config/SecurityConfig.java
@@ -6,18 +6,20 @@ import com.carrot.auth.service.CustomOAuth2UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @RequiredArgsConstructor
-//@EnableWebSecurity
+@EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     private final CustomOAuth2UserService customOAuth2UserService;
 
     private final String[] authOnly = new String[]{};
-    private static final String[] permitAll = new String[]{
+    private static final String[] availableUriArray = new String[]{
             "/",
             "/css/**",
             "/images/**",
@@ -46,7 +48,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
                 .authorizeRequests()
                 //permit
-                .antMatchers(permitAll)
+                .antMatchers(availableUriArray)
                 .permitAll()
                 //auth need
                 .antMatchers(authOnly)
@@ -68,4 +70,18 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
+
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring()
+                .antMatchers(
+                        "/css/**",
+                        "/js/**",
+                        "/img/**",
+                        "/images/**",
+                        "/"
+                );
+    }
+
 }
